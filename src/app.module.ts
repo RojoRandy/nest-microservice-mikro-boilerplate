eed8abc81a6b6e33fe93acb637c66ed473f8c1a7
RandyRojo <rojo.q.randy@gmail.com>
2025-05-29 16:26:13 -0600

    refactor: remove AplicacionesModule import and delete e2e tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { AplicacionesModule } from './modules/aplicaciones/aplicaciones.module';
 
 @Module({
   imports: [
     MikroOrmModule.forRoot(),
-    AplicacionesModule
   ],
 })
 export class AppModule {
